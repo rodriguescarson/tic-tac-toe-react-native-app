2ed21d8b21f29251812f7733240c926a7f1bf5e6
Carson <rodriguescarson@hotmail.com>
2022-03-05 22:40:00 +0530

    section 5 full

--- src/screens/game/game.tsx
+++ src/screens/game/game.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React, { ReactElement } from "react";
 import styles from "./game.styles";
 import { StackNavigationProp } from "@react-navigation/stack";
@@ -10,6 +10,12 @@ export default function Game({ navigation }: GameProps): ReactElement {
     return (
         <View style={styles.container}>
             <Text>Game</Text>
+            <Button
+                title="Home"
+                onPress={() => {
+                    navigation.navigate("Home");
+                }}
+            />
         </View>
     );
 }
